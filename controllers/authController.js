eacc4008fc2f2a3891c34040fc636ca5dd3130c4
fortunekenny <gbephraim@gmail.com>
2023-08-26 06:29:22 +0100

    in development

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,29 @@
 const User = require("../model/user");
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
+const { attachCookiesToResponse } = require("../utils");
 
 const register = async (req, res) => {
-  const user = await User.create(req.body);
-  res.status(StatusCodes.CREATED).json({ user });
+  const { email, name, password } = req.body;
+  const emailAlreadyExists = await User.findOne({ email });
+  if (emailAlreadyExists) {
+    throw new CustomError.BadRequestError("Email already exist");
+  }
+
+  //first registered user is
+  const isFirstAccount = (await User.countDocuments({})) === 0;
+  const role = isFirstAccount ? "founder" : "user";
+
+  const user = await User.create({ email, name, password, role });
+  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+  attachCookiesToResponse({ res, user: tokenUser });
+  res.status(StatusCodes.CREATED).json({ user: tokenUser });
 };
+
 const login = async (req, res) => {
   res.send("login user");
 };
+
 const logout = async (req, res) => {
   res.send("logout user");
 };
