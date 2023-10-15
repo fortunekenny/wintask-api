e578cc8e12aecbe269ed9355d547092754758954
fortunekenny <gbephraim@gmail.com>
2023-10-15 22:42:02 +0100

    authentication

--- middleware/authenctication.js
+++ middleware/authenctication.js
@@ -2,6 +2,39 @@ const { UnauthenticatedError, UnauthorizedError } = require("../errors");
 const { isTokenValid } = require("../utils");
 
 const authenticateUser = async (req, res, next) => {
+  let token;
+  // check header
+  const authHeader = req.headers.authorization;
+  if (authHeader && authHeader.startsWith("Bearer")) {
+    token = authHeader.split(" ")[1];
+  }
+  // check cookies
+  else if (req.cookies.token) {
+    token = req.signedCookies.token;
+    // token = req.cookies.token;
+  }
+
+  if (!token) {
+    throw new CustomError.UnauthenticatedError("Authentication invalid");
+  }
+  try {
+    const payload = isTokenValid(token);
+
+    // Attach the user and his permissions to the req object
+    req.user = {
+      userId: payload.user.userId,
+      name: payload.user.name,
+      role: payload.user.role,
+    };
+
+    next();
+  } catch (error) {
+    throw new CustomError.UnauthenticatedError("Authentication invalid");
+  }
+};
+
+/*
+const authenticateUser = async (req, res, next) => {
   const token = req.signedCookies.token;
 
   if (!token) {
@@ -15,6 +48,7 @@ const authenticateUser = async (req, res, next) => {
     throw new UnauthenticatedError("Authentication invalid");
   }
 };
+*/
 
 const authorizePermissions = (...roles) => {
   return (req, res, next) => {
