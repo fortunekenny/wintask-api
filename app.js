6e80cde73fd3f46d74ea8fe15ec193c2564fb36f
fortunekenny <gbephraim@gmail.com>
2023-10-15 01:51:33 +0100

    authentication

--- app.js
+++ app.js
@@ -25,6 +25,10 @@ const taskRouter = require("./routes/taskRoutes");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+//IMPORTING ENDS
+
+app.use(express.json());
+
 app.set("trust proxy", 1);
 app.use(
   rateLimiter({
@@ -32,15 +36,20 @@ app.use(
     max: 60,
   })
 );
+
 app.use(helmet());
 app.use(cors());
 app.use(xss());
 app.use(mongoSanitize());
 
-app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 
 app.use(express.static("public"));
+
+// app.get("/api/v1", (req, res) => {
+//   console.log(req.signedCookies);
+//   res.send("home page");
+// });
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
@@ -50,6 +59,7 @@ app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 8000;
+
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL);
--- middleware/authenctication.js
+++ middleware/authenctication.js
@@ -2,15 +2,30 @@ const CustomError = require("../errors");
 const { isTokenValid } = require("../utils");
 
 const authenticateUser = async (req, res, next) => {
-  const token = req.signedCookies.token;
+  let token;
+  // check header
+  const authHeader = req.headers.authorization;
+  if (authHeader && authHeader.startsWith("Bearer")) {
+    token = authHeader.split(" ")[1];
+  }
+  // check cookies
+  else if (req.cookies.token) {
+    token = req.cookies.token;
+  }
 
   if (!token) {
     throw new CustomError.UnauthenticatedError("Authentication invalid");
   }
-
   try {
-    const { name, userId, role } = isTokenValid({ token });
-    req.user = { name, userId, role };
+    const payload = isTokenValid(token);
+
+    // Attach the user and his permissions to the req object
+    req.user = {
+      userId: payload.user.userId,
+      name: payload.user.name,
+      role: payload.user.role,
+    };
+
     next();
   } catch (error) {
     throw new CustomError.UnauthenticatedError("Authentication invalid");
