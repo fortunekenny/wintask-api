04c7d04b6f94c93c7d7bbba268c253c6b615e70c
fortunekenny <gbephraim@gmail.com>
2023-10-15 03:11:54 +0100

    authentication

--- middleware/authenctication.js
+++ middleware/authenctication.js
@@ -2,33 +2,17 @@ const CustomError = require("../errors");
 const { isTokenValid } = require("../utils");
 
 const authenticateUser = async (req, res, next) => {
-  let token;
-  // check header
-  const authHeader = req.headers.authorization;
-  if (authHeader && authHeader.startsWith("Bearer")) {
-    token = authHeader.split(" ")[1];
-  }
-  // check cookies
-  else if (req.cookies.token) {
-    token = req.cookies.token;
-  }
+  const token = req.signedCookies.token;
 
   if (!token) {
-    throw new CustomError.UnauthenticatedError("Authentication invalid");
+    throw new UnauthenticatedError("Authentication invalid");
   }
   try {
-    const payload = isTokenValid(token);
-
-    // Attach the user and his permissions to the req object
-    req.user = {
-      userId: payload.user.userId,
-      name: payload.user.name,
-      role: payload.user.role,
-    };
-
+    const { name, userId, role } = isTokenValid({ token });
+    req.user = { name, userId, role };
     next();
   } catch (error) {
-    throw new CustomError.UnauthenticatedError("Authentication invalid");
+    throw new UnauthenticatedError("Authentication invalid");
   }
 };
 
